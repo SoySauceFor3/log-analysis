fe6dfeb3104724e47810dd67c8155e65507ee486
Xinya Yang <xinyayang0506@gmail.com>
2020-10-09 23:36:11 -0500

    make two commands

--- src/extension.ts
+++ src/extension.ts
@@ -5,78 +5,93 @@ import * as csvParse from "csv-parse/lib/sync";
 import { Filter, filterLines } from "./utils";
 import { FocusFoldingRangeProvider } from "./foldingrangeprovider";
 
+async function init(uri: vscode.Uri): Promise<Filter[]> {
+    let textDoc = await vscode.workspace.openTextDocument(uri);
+    let output: string[][] = csvParse(textDoc.getText());
+    return output.map((row) => ({
+        isHighlighted: row[0] === "T",
+        isShown: row[1] === "T",
+        regex: new RegExp(row[2]),
+        color: row[3],
+    }));
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log(
-        'Congratulations, your extension "log-analysis" is now active!'
+    //read the config file
+    let uri = vscode.Uri.file(
+        "/Users/xinya/Documents/GitHub/log-analysis/src/example.csv"
     );
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand(
-        "log-analysis.addFilter",
-        () => {
-            // The code you place here will be executed every time your command is executed
 
-            let uri = vscode.Uri.file(
-                "/Users/xinya/Documents/GitHub/log-analysis/src/example.csv"
-            );
-            vscode.workspace.fs.readFile(uri).then(console.log, console.log);
-            vscode.workspace
-                .openTextDocument(uri)
-                .then((textDoc) => {
-                    let output: string[][] = csvParse(textDoc.getText());
-                    return output.map((row) => ({
-                        isHighlighted: row[0] === "T",
-                        isShown: row[1] === "T",
-                        regex: new RegExp(row[2]),
-                        color: row[3],
-                    }));
-                }, console.log)
-                .then((filterArr) => {
-                    let editor = vscode.window.activeTextEditor!;
-                    let sourceCode = editor.document.getText();
-                    const sourceCodeArr = sourceCode.split("\n");
-                    filterArr.forEach((filter) => {
-                        if (!filter.isHighlighted) {
-                            return;
-                        }
-                        const lineNumbers = filterLines(sourceCodeArr, filter);
-                        const decorationsArray = lineNumbers.map((lineIdx) => {
-                            return new vscode.Range(
-                                new vscode.Position(lineIdx, 0),
-                                new vscode.Position(lineIdx, 20)
-                            );
-                        });
-                        let decorationType = vscode.window.createTextEditorDecorationType(
-                            {
-                                backgroundColor: filter.color,
-                                isWholeLine: true,
+    let inFocusMode = false;
+
+    init(uri).then(filterArr => {
+        // register commands
+        let disposable = vscode.commands.registerCommand(
+            "log-analysis.applyHighlight",
+            () => {
+                init(uri)
+                    .then((filterArr) => {
+                        let editor = vscode.window.activeTextEditor!;
+                        let sourceCode = editor.document.getText();
+                        const sourceCodeArr = sourceCode.split("\n");
+                        filterArr.forEach((filter) => {
+                            if (!filter.isHighlighted) {
+                                return;
                             }
+                            const lineNumbers = filterLines(sourceCodeArr, filter);
+                            const decorationsArray = lineNumbers.map((lineIdx) => {
+                                return new vscode.Range(
+                                    new vscode.Position(lineIdx, 0),
+                                    new vscode.Position(lineIdx, 20)
+                                );
+                            });
+                            let decorationType = vscode.window.createTextEditorDecorationType(
+                                {
+                                    backgroundColor: filter.color,
+                                    isWholeLine: true,
+                                }
+                            );
+                            editor.setDecorations(decorationType, decorationsArray);
+                            // decorationType.dispose();
+                        });
+                        vscode.languages.registerFoldingRangeProvider(
+                            {
+                                pattern: "*",
+                            },
+                            new FocusFoldingRangeProvider(filterArr)
                         );
-                        editor.setDecorations(decorationType, decorationsArray);
-                        // decorationType.dispose();
-                    });
-                    vscode.languages.registerFoldingRangeProvider(
-                        {
-                            pattern: "*",
-                        },
-                        new FocusFoldingRangeProvider(filterArr)
-                    );
+                        vscode.commands.executeCommand("editor.foldAll");
+                    }, console.log)
+                    .then(() => {
+                        console.log("FINISHED");
+                    }, console.log);
+
+                //
+            }
+        );
+
+        context.subscriptions.push(disposable);
+
+        let disposable2 = vscode.commands.registerCommand(
+            "log-analysis.toggleFocusMode",
+            () => {
+                if (inFocusMode) {
+                    //toggle off focus mode, so unfold everything
+                    vscode.commands.executeCommand("editor.unfoldAll");
+                } else {
+                    //toggle on focus mode, so fold everything
                     vscode.commands.executeCommand("editor.foldAll");
-                }, console.log)
-                .then(() => {
-                    console.log("FINISHED");
-                }, console.log);
+                    
+                }
+                inFocusMode = !inFocusMode;
 
-            //
-        }
-    );
-
-    context.subscriptions.push(disposable);
+            })
+        ;
+        context.subscriptions.push(disposable2);
+            
+    });
 }
 
 // this method is called when your extension is deactivated
